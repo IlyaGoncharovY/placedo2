19e00bc692c8c85a25ac63029ec42db182aa6e8c
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-08-16 01:39:25 +0300

    fix(FieldTime) use custom hook in component

--- src/RNCore/components/fields/FieldTime.tsx
+++ src/RNCore/components/fields/FieldTime.tsx
@@ -1,5 +1,11 @@
-import {TextInput} from "react-native";
+import React from "react";
+import {FontAwesome} from '@expo/vector-icons';
+import {TextInput, TouchableOpacity} from "react-native";
+
 import tw from "../../libs/tailwind";
+import {Row1, RowCenter} from "../markup/markup";
+import {useFieldTime} from "../../../common/customHook/useFieldTime";
+
 
 interface ITimeField {
     class?: string // tailwind classes
@@ -9,8 +15,46 @@ interface ITimeField {
 }
 
 export function FieldTime(props: ITimeField) {
-    return <TextInput
-        style={tw`${props.class || ''}`}
-    />
+
+    const {
+        inputValue,
+        setInputValue,
+        handleBlur,
+        incrementHour,
+        decrementHour,
+        onFocusHandler
+    } = useFieldTime(props.value, props.onChange)
+
+    return (
+        <RowCenter>
+            {props.useButtons ? (
+                <Row1>
+                    <TouchableOpacity onPress={decrementHour}>
+                        <FontAwesome name="minus" size={20} style={tw`p-2`}/>
+                    </TouchableOpacity>
+                    <TextInput
+                        style={tw`${props.class || ''}`}
+                        value={inputValue}
+                        onChangeText={(text) => setInputValue(text)}
+                        onBlur={() => handleBlur(inputValue)}
+                        keyboardType="numeric"
+                        onFocus={onFocusHandler}
+                    />
+                    <TouchableOpacity onPress={incrementHour}>
+                        <FontAwesome name="plus" size={20} style={tw`p-2`}/>
+                    </TouchableOpacity>
+                </Row1>
+            ) : (
+                <TextInput
+                    style={tw`${props.class || ''}`}
+                    value={inputValue}
+                    onChangeText={(text) => setInputValue(text)}
+                    onBlur={() => handleBlur(inputValue)}
+                    keyboardType="numeric"
+                    onFocus={onFocusHandler}
+                />
+            )}
+        </RowCenter>
+    );
 }
 
